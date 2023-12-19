02c91f69025924ce30a0992d89389945219be3d2
Kunal Yadav <83703416+kunal-yadav-404@users.noreply.github.com>
2023-12-19 14:24:59 +0000

    controllers added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/register", (req, res) => {
-  res.send("register user");
-});
+// controllers
+import { register } from "../controllers/auth";
+
+router.get("/register", register);
 
 module.exports = router;
