16e07ee13561a4fd4602918adecd5695cf5c7afd
Kunal Yadav <yadavkunal780@gmail.com>
2023-12-20 21:46:51 +0530

    login

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -17,5 +17,5 @@ export const hashPassword = (password) => {
 };
 
 export const comparePassword = (password, hashed) => {
-  return bcrypt.compare(password, hashed);
+  return bcrypt.compare(password, hashed); // boolean
 };
