388ad46f1a808449ffb839c6778ce6ade69b2fd8
Kunal Yadav <yadavkunal780@gmail.com>
2023-12-22 10:26:09 +0530

    state change

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,8 @@
 const Index = () => {
   return (
     <>
-      <h1 className="jumbotron text-center bg-primary square">
-        Online Education Marketplace
-      </h1>
-      <p>from next js...</p>
+      <h1 className="jumbotron text-center bg-primary square">Peer To Peer</h1>
+      <p>yet to implement...</p>
     </>
   );
 };
